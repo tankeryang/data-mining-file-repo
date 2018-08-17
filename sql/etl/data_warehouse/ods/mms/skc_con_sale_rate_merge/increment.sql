4b2f7ecbbe4f2c287a71438f26604f7a63b705a1
tankeryang <youngzyang@outlook.com>
2018-08-17 12:15:24 +0800

    update: add lot of feature

--- sql/etl/data_warehouse/ods/mms/skc_con_sale_rate_merge/increment.sql
+++ sql/etl/data_warehouse/ods/mms/skc_con_sale_rate_merge/increment.sql
@@ -1,55 +1,70 @@
 INSERT INTO ods_mms.skc_con_sale_rate_merge
     SELECT
-        mso.sales_order_no                                                         sales_order_no,
-        substr(msoi.sku_code, 1, 13)                                               skc_code,
-        base_str_info.city_name                                                    city_name,
-        mso.store_code                                                             terminal_store_code,
-        date_format(mso.audit_time, '%Y-%m-%d')                                    audit_date,
-        cast(year(mso.audit_time) AS VARCHAR)                                      year_code,
-        IF(
-            month(mso.audit_time) < 10,
-            concat('0', cast(month(mso.audit_time) AS VARCHAR)),
-            cast(month(mso.audit_time) AS VARCHAR)
-        )                                                                          month_code,
+        mso.store_code  store_code,
+        substr(msoi.sku_code, 1, 13)  skc_code,
+        IF(roa.repeat_order_amount IS NOT NULL, roa.repeat_order_amount, 0)  repeat_order_amount,
+        IF(aoa.all_order_amount IS NOT NULL, aoa.all_order_amount, 0)  all_order_amount,
+        cast(year(mso.audit_time) AS VARCHAR)  year_code,
         IF(
             week(mso.audit_time) < 10,
             concat('0', cast(week(mso.audit_time) AS VARCHAR)),
             cast(week(mso.audit_time) AS VARCHAR)
-        )                                                                          week_code,
-        ot.order_ntype                                                             order_ntype
+        )  week_code,
+        date_format(mso.audit_time, '%Y-%m-%d')  audit_date
     FROM
         cdm_ipos.merged_sales_order mso
     LEFT JOIN
         cdm_ipos.merged_sales_order_item msoi
     ON
         mso.sales_order_no = msoi.sales_order_no
-    LEFT JOIN
-        cdm_base.store_info base_str_info
-    ON
-        mso.store_code = base_str_info.store_code
-    RIGHT JOIN (
+    
+    -- aoa 全单数量
+    LEFT JOIN (
         SELECT
-            mso.sales_order_no,
+            mso.store_code  store_code,
+            substr(msoi.sku_code, 1, 13)  skc_code,
+            cast(count(mso.sales_order_no) AS INTEGER)  all_order_amount,
+            date_format(mso.audit_time, '%Y-%m-%d')  audit_date
+        FROM
+            cdm_ipos.merged_sales_order mso
+        LEFT JOIN
+            cdm_ipos.merged_sales_order_item msoi
+        ON
+            mso.sales_order_no = msoi.sales_order_no
+        GROUP BY
             mso.store_code,
-            date_format(mso.audit_time, '%Y-%m-%d')            audit_date,
-            count(msoi.sku_code)                               order_sku_num,
-            cast(IF(count(msoi.sku_code)=1, 0, 1) AS TINYINT)  order_ntype
-        FROM 
+            substr(msoi.sku_code, 1, 13),
+            date_format(mso.audit_time, '%Y-%m-%d')
+    ) aoa
+    ON
+        mso.store_code = aoa.store_code
+        AND substr(msoi.sku_code, 1, 13) = aoa.skc_code
+        AND date_format(mso.audit_time, '%Y-%m-%d') = aoa.audit_date
+
+    -- roa 多件商品的单数量
+    LEFT JOIN (
+        SELECT
+            mso.store_code  store_code,
+            substr(msoi.sku_code, 1, 13)  skc_code,
+            cast(count(mso.sales_order_no) AS INTEGER)  repeat_order_amount,
+            date_format(mso.audit_time, '%Y-%m-%d')  audit_date
+        FROM
             cdm_ipos.merged_sales_order mso
         LEFT JOIN
             cdm_ipos.merged_sales_order_item msoi
         ON
             mso.sales_order_no = msoi.sales_order_no
         WHERE
-            mso.order_type = 1
-        GROUP BY 
-            mso.sales_order_no,
+            mso.order_item_quantity > 1
+        GROUP BY
             mso.store_code,
+            substr(msoi.sku_code, 1, 13),
             date_format(mso.audit_time, '%Y-%m-%d')
-    ) ot
+    ) roa
     ON
-        mso.sales_order_no = ot.sales_order_no
-        AND mso.store_code = ot.store_code
-        AND date_format(mso.audit_time, '%Y-%m-%d') = ot.audit_date
+        mso.store_code = roa.store_code
+        AND substr(msoi.sku_code, 1, 13) = roa.skc_code
+        AND date_format(mso.audit_time, '%Y-%m-%d') = roa.audit_date
     WHERE
-        date_format(mso.audit_time, '%Y-%m-%d') > (SELECT max(audit_date) FROM ods_mms.skc_con_sale_rate_merge);
+        date_format(mso.audit_time, '%Y-%m-%d') IS NOT NULL;
+        AND date_format(mso.audit_time, '%Y-%m-%d') > (SELECT max(audit_date) FROM ods_mms.skc_con_sale_rate_merge);
